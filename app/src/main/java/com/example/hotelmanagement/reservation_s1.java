e27935abcc891f91ec642a66f21a7772fd5e1748
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-08-03 11:22:45 +0530

    Add reservation page

---
+++ app/src/main/java/com/example/hotelmanagement/reservation_s1.java
@@ -0,0 +1,14 @@
+package com.example.hotelmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservation_s1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_s1);
+    }
+}
