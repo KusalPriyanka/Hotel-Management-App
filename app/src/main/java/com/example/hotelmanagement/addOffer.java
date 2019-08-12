3472a345f67a2c74f0e2441177b67c0d55609cd3
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-08-12 21:21:02 +0530

    Add offers page

---
+++ app/src/main/java/com/example/hotelmanagement/addOffer.java
@@ -0,0 +1,14 @@
+package com.example.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class addOffer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_offer);
+    }
+}
