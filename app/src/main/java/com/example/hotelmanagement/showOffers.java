6418c95e1f36ebac95d76422560f27292051e786
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-08-12 21:21:21 +0530

    Add show offers

---
+++ app/src/main/java/com/example/hotelmanagement/showOffers.java
@@ -0,0 +1,14 @@
+package com.example.hotelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showOffers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_offers);
+    }
+}
