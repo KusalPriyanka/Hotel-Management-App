03c6c1a11b77d33dde0e3911b14e43be58b5732a
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-09-08 23:23:04 +0530

    Update signup activity

--- app/src/main/java/com/example/hotelmanagement/signup.java
+++ app/src/main/java/com/example/hotelmanagement/signup.java
@@ -3,12 +3,23 @@ package com.example.hotelmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class signup extends AppCompatActivity {
+
+    private EditText userName, mobileNo, email, password;
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        userName = findViewById(R.id.userName);
+        mobileNo = findViewById(R.id.mobileNo);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+
     }
 }
