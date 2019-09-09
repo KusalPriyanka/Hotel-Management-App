c7c1957fbe1612e0a1e6bb14d16f95d17a54658f
AshenSenevirathne <ashensachin@gmail.com>
2019-09-09 17:22:38 +0530

    MM_Main_Meals Added

---
+++ app/src/main/java/com/example/hotelmanagement/MM_Main_Meals.java
@@ -0,0 +1,14 @@
+package com.example.hotelmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MM_Main_Meals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mm__main__meals);
+    }
+}
