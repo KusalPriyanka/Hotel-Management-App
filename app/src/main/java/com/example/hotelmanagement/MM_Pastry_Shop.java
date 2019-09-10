31c5d919c9324445a7a06c18341853ed2fc3ea06
AshenSenevirathne <ashensachin@gmail.com>
2019-09-10 09:43:13 +0530

    Pastry Shop view Created

--- app/src/main/java/com/example/hotelmanagement/MM_Pastry_Shop.java
+++ app/src/main/java/com/example/hotelmanagement/MM_Pastry_Shop.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MM_Pastry_Shop extends AppCompatActivity {
 
     Button mainMeals, pastryShop;
+    ViewFlipper vf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +35,12 @@ public class MM_Pastry_Shop extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+
     }
+
+
+
 }
