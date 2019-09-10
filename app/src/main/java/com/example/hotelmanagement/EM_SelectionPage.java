ce6797a13fda2f69560cde7fca76c8338ed20627
AnjaniWelagedara <anjanwelagedara3@gmail.com>
2019-09-10 15:44:41 +0530

    Completed EventmanagementHome

---
+++ app/src/main/java/com/example/hotelmanagement/EM_SelectionPage.java
@@ -0,0 +1,14 @@
+package com.example.hotelmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EM_SelectionPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_em__selection_page);
+    }
+}
