8f4410fd7ca1ae3858f46e982b11ace70a7f5d9f
AshenSenevirathne <ashensachin@gmail.com>
2019-09-10 08:23:01 +0530

    MM_Pastry_Shop Activity Linked

--- app/src/main/java/com/example/hotelmanagement/MM_Pastry_Shop.java
+++ app/src/main/java/com/example/hotelmanagement/MM_Pastry_Shop.java
@@ -2,26 +2,24 @@ package com.example.hotelmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
-public class MM_Main_Meals extends AppCompatActivity {
+public class MM_Pastry_Shop extends AppCompatActivity {
 
     Button mainMeals, pastryShop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mm__main__meals);
+        setContentView(R.layout.activity_mm__pastry__shop);
 
         mainMeals = findViewById(R.id.mainMeal);
         mainMeals.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MM_Main_Meals.this, MM_Main_Meals.class);
+                Intent intent = new Intent(MM_Pastry_Shop.this, MM_Main_Meals.class);
                 startActivity(intent);
             }
         });
@@ -30,13 +28,9 @@ public class MM_Main_Meals extends AppCompatActivity {
         pastryShop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MM_Main_Meals.this, MM_Pastry_Shop.class);
+                Intent intent = new Intent(MM_Pastry_Shop.this, MM_Pastry_Shop.class);
                 startActivity(intent);
             }
         });
-
-
     }
-
-
 }
