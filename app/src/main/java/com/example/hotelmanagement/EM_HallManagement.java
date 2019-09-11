f43497becabb00798b7a418554fd84cb31b05e91
AnjaniWelagedara <anjanwelagedara3@gmail.com>
2019-09-11 14:48:24 +0530

    Created data adding section

---
+++ app/src/main/java/com/example/hotelmanagement/EM_HallManagement.java
@@ -0,0 +1,68 @@
+package com.example.hotelmanagement;
+
+public class EM_HallManagement {
+
+    private String id;
+    private String name;
+    private Float price;
+    private String description;
+    private  boolean wedding;
+    private  boolean events;
+    private String imageName;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isWedding() {
+        return wedding;
+    }
+
+    public void setWedding(boolean wedding) {
+        this.wedding = wedding;
+    }
+
+    public boolean isEvents() {
+        return events;
+    }
+
+    public void setEvents(boolean events) {
+        this.events = events;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+}
