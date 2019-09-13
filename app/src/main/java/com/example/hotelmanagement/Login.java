a9919105616f98e3321dac8b4d23b309fad6010f
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-09-13 22:41:39 +0530

    Update Login interface using Firebase Authentication

--- app/src/main/java/com/example/hotelmanagement/Login.java
+++ app/src/main/java/com/example/hotelmanagement/Login.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,6 +19,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import Modal.Customer;
 
@@ -28,7 +34,6 @@ public class Login extends AppCompatActivity {
     private EditText username, password;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,9 +64,7 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //LoginUser();
-                Intent intent = new Intent(Login.this,Login.class);
-                startActivity(intent);
+                LoginUser();
             }
         });
 
@@ -99,21 +102,15 @@ public class Login extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
 
                             if(!task.isSuccessful()){
-
-                                Toast.makeText(getApplicationContext(), "Sign In Problem!" ,Toast.LENGTH_LONG).show();
-
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage()  ,Toast.LENGTH_LONG).show();
                             }
 
                             else {
-
-                                FirebaseUser user = mAuth.getCurrentUser();
-
-                                Toast.makeText(getApplicationContext(), user.getUid() ,Toast.LENGTH_LONG).show();
-
+                                startActivity(new Intent(Login.this,MainActivity.class));
                             }
                         }
                     });
         }
-
     }
+
 }
