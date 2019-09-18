2c453af7f65ed7357b3a9649922783577d8d731e
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-09-18 22:54:31 +0530

    Initialize the Elements in Offer activity

--- app/src/main/java/com/example/hotelmanagement/addOffer.java
+++ app/src/main/java/com/example/hotelmanagement/addOffer.java
@@ -3,12 +3,20 @@ package com.example.hotelmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class addOffer extends AppCompatActivity {
+
+    private EditText offerName, offerDes, offerPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_offer);
+
+        offerName = findViewById(R.id.offerName);
+        offerDes = findViewById(R.id.offerdescription);
+        offerPrice = findViewById(R.id.price);
+
     }
 }
