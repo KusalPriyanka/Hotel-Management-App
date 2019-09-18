ba0c795dfbc77adf4b956be996765bb208d58b87
AnjaniWelagedara <anjanwelagedara3@gmail.com>
2019-09-18 12:40:15 +0530

    Search bar added

--- app/src/main/java/com/example/hotelmanagement/MM_Main_Meals.java
+++ app/src/main/java/com/example/hotelmanagement/MM_Main_Meals.java
@@ -37,7 +37,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
+//import com.squareup.picasso.Picasso;
 
 
 import java.util.ArrayList;
@@ -202,7 +202,7 @@ public class MM_Main_Meals extends AppCompatActivity {
                         if(mainMeals.isDinner() == true){
                             dn.setCheckMarkDrawable(R.drawable.check_view);
                         }
-                        Picasso.get().load(mainMeals.getImageName()).into(image);
+                     //   Picasso.get().load(mainMeals.getImageName()).into(image);
 
 
                     }
