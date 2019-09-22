5cda29c800bc3f10f465a000c3a61dbea2fb2376
Kusal Priyanka <kusalpriyanka782@gmail.com>
2019-09-22 14:29:08 +0530

    Add Admin Dashboard

---
+++ app/src/main/java/com/example/hotelmanagement/reservationlist.java
@@ -0,0 +1,14 @@
+package com.example.hotelmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservationlist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservationlist);
+    }
+}
